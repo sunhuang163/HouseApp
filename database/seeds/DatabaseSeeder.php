930c5b972275ab2e0a7e46cc630bc79c9f8f1144
Sunhuang2005 <sunhuang2005@163.com>
2017-07-18 20:00:12 +0800

    Push to Github

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // $this->call(DepartmentSeed::class);
-        // $this->call(TitleSeed::class);
+        $this->call(DepartmentSeed::class);
+        $this->call(TitleSeed::class);
         $this->call(SexSeed::class);
     }
 }
